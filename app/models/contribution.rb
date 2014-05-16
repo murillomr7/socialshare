56fa934853dd6c88cb6db660aef131a6e26ff0b9
Diego Souza Rodrigues <diegosouzapb@gmail.com>
2014-05-16 02:22:51 -0300

    adequating law of demeter

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,6 @@
 class Contribution < ActiveRecord::Base
   belongs_to :share_request
   belongs_to :member
+  delegate :name, to: :member, :prefix => true, :allow_nil => true
+  delegate :title, to: :social_share, :prefix => true, :allow_nil => true
 end
